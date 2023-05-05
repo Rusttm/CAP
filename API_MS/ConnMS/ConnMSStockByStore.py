bc403527a55d4c49119672131bd25ca074799a92
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-05-05 22:14:53 +0300

    stockRemains by stores  commit

--- API_MS/ConnMS/ConnMSStockByStore.py
+++ API_MS/ConnMS/ConnMSStockByStore.py
@@ -2,7 +2,7 @@ from API_MS.ConnMS.ConnMSMainClass import ConnMSMainClass
 
 
 class ConnMSStockByStore(ConnMSMainClass):
-    """ connector to MoiSklad stock remains(ненулевые остатки) by stores"""
+    """ connector to MoiSklad stock remains(ненулевые остатки) by store"""
 
     def __init__(self):
         super().__init__()
@@ -13,31 +13,38 @@ class ConnMSStockByStore(ConnMSMainClass):
         """ sets api_url and api_token from config file"""
         conf = self.get_config_data()
         if conf:
-            self.set_api_url(conf['MoiSklad']['url_store_stores'])
+            self.set_api_url(conf['MoiSklad']['url_stock_stores'])
             self.set_api_token(conf['MoiSklad']['access_token'])
         else:
-            self.logger.warning("cant get info from configfile url_balance or access_token")
+            self.logger.warning("cant get info from configfile url or access_token")
 
-    def get_stock_remains(self, to_file=False):
-        """ return dict with stock remains"""
-        data_dict = self.get_api_data(to_file=to_file)
-        return data_dict
+    def get_stock_by_store(self, to_date=None, to_file=False):
+        """ return dict with stock by store remains"""
+        param = ""
+        if to_date:
+            param += f"filter=moment={to_date}"
+        else:
+            self.logger.warning("Stock remains by store request not specified to_date parameter")
+            return self.get_api_data(to_file=to_file)
+        self.set_api_param_line(param)
+        new_data_dict = self.get_api_data(to_file=to_file)
+        return new_data_dict
 
-    def get_stock_remains_sum(self):
-        data_dict = self.get_api_data()
+    def get_stock_by_store_sum(self, to_date=None):
+        """ return dict {store:sum}"""
+        data_dict = self.get_stock_by_store(to_date=to_date)
+        result_array = dict()
         result_sum = 0
         if data_dict:
             for prod in data_dict['rows']:
                 try:
                     result_sum += prod['stock'] * prod['price']
                 except Exception as e:
-                    self.logger.warning(f"for {prod['name']} summ not considered")
+                    self.logger.warning(f"for {prod['name']} summ not considered. error {e}")
         return result_sum
 
 
-
-
-
-
 if __name__ == '__main__':
-    connector = ConnMSStockRemains()
+    connector = ConnMSStockByStore()
+    data = connector.get_stock_by_store(to_file=True)
+    print(data)
--- API_MS/ConnMS/ConnMSStockRemains.py
+++ API_MS/ConnMS/ConnMSStockRemains.py
@@ -16,7 +16,7 @@ class ConnMSStockRemains(ConnMSMainClass):
             self.set_api_url(conf['MoiSklad']['url_stock_all'])
             self.set_api_token(conf['MoiSklad']['access_token'])
         else:
-            self.logger.warning("cant get info from configfile url_balance or access_token")
+            self.logger.warning("cant get info from configfile url or access_token")
 
     def get_stock_remains(self, to_date=None, to_file=False):
         """ return dict with stock remains"""
@@ -25,7 +25,7 @@ class ConnMSStockRemains(ConnMSMainClass):
             param += f"filter=moment={to_date}"
         else:
             self.logger.warning("StockRemains request not specified to_date parameter")
-            return self.get_api_data()
+            return self.get_api_data(to_file=to_file)
         self.set_api_param_line(param)
         new_data_dict = self.get_api_data(to_file=to_file)
         return new_data_dict
@@ -38,7 +38,7 @@ class ConnMSStockRemains(ConnMSMainClass):
                 try:
                     result_sum += prod['stock'] * prod['price']
                 except Exception as e:
-                    self.logger.warning(f"for {prod['name']} summ not considered")
+                    self.logger.warning(f"for {prod['name']} summ not considered. error {e}")
         return result_sum
 
 
