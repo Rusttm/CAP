a11aa478634b915abb714474558112b365def6be
rusttm <rustammazhatov@gmail.com>
2023-05-11 21:17:59 +0100

    connector changes

--- API_Docker/ConnDocker/ConnDCTest.py
+++ API_Docker/ConnDocker/ConnDCTest.py
@@ -1,11 +1,15 @@
 from API_Docker.ConnDocker.ConnDCMainClass import ConnDCMainClass
 import docker
 class ConnDCTest(ConnDCMainClass):
-
+    client = None
     def __init__(self):
         super().__init__()
+        self.client = docker.from_env()
+
+    def get_cont_list(self):
+        return self.client.containers.list()
 
 
 if __name__ == '__main__':
     connector = ConnDCTest()
-    # connector.get_invin_filtered_by_date(from_date="2023-01-01", to_date="2023-02-01", to_file=True)
+    print(connector.get_cont_list())
