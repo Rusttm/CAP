6a9430da36791aae59ea822b2e076169473c7069
rusttm <rustammazhatov@gmail.com>
2023-05-11 22:37:34 +0100

    sql connector postgresql commit

--- Pgsql/ConnPgsql/ConnPsqlInit.py
+++ Pgsql/ConnPgsql/ConnPsqlInit.py
@@ -1,6 +1,23 @@
-from Pgsql.ConnPgsql.ConnPsqlMainClass import CAPMainClass
+from Pgsql.ConnPgsql.ConnPsqlMainClass import ConnPsqlMainClass
+import psycopg2
 
-
-class ConnPsqlInit(CAPMainClass):
+class ConnPsqlInit(ConnPsqlMainClass):
+    pgsql_conn = None
     def __init__(self):
         super().__init__()
+        # self.pgsql_conn = psycopg2.connect("dbname=capdb user=capuser password=cap_pass")
+        self.pgsql_conn = psycopg2.connect(
+            host="localhost:5432",
+            database="capdb",
+            user="capuser",
+            password="cap_pass")
+        self.cursor = self.pgsql_conn.cursor()
+
+    def get_pgsql_version(self):
+        self.cursor.execute('SELECT version()')
+        db_version = self.cursor.fetchone()
+        print(db_version)
+
+if __name__ == '__main__':
+    connector = ConnPsqlInit()
+    connector.get_pgsql_version()
