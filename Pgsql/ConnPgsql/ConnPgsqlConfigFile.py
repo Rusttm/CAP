af98f475e7b574b742ca4832ac069687dfd8deca
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-05-12 02:26:11 +0300

    config for sql commit

---
+++ Pgsql/ConnPgsql/ConnPgsqlConfigFile.py
@@ -0,0 +1,27 @@
+# from Main.CAPMainClass import CAPMainClass
+import os
+import configparser
+
+
+class ConnPgsqlConfigFile(object):
+    """ configfile connector"""
+    dir_name = "config"
+    file_name = "sqlconfig.ini"
+
+
+    def get_config_data(self, sector='POSTGRESQL'):
+        """ extract data from sql config file
+        return config section POSTGRESQL """
+        try:
+            conf = configparser.ConfigParser()
+            file = os.path.dirname(os.path.dirname(__file__))
+            CONF_FILE_PATH = os.path.join(file, self.dir_name, self.file_name)
+            conf.read(CONF_FILE_PATH)
+            return conf[sector]
+        except Exception as e:
+            return None
+
+
+if __name__ == '__main__':
+    connector = ConnPgsqlConfigFile()
+    print(connector.get_config_data())
