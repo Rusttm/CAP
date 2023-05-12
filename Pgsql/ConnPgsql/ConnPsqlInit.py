ec5de3a2fbcc0d7b0d65beb727cca7d8e7cde909
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-05-12 03:08:45 +0300

    config for sql commit

--- Pgsql/ConnPgsql/ConnPsqlInit.py
+++ Pgsql/ConnPgsql/ConnPsqlInit.py
@@ -2,25 +2,31 @@ from Pgsql.ConnPgsql.ConnPsqlMainClass import ConnPsqlMainClass
 from Pgsql.ConnPgsql.ConnPgsqlConfig import ConnPgsqlConfig
 import psycopg2
 
+
 class ConnPsqlInit(ConnPsqlMainClass, ConnPgsqlConfig):
     pgsql_conn = None
+
     def __init__(self):
         super().__init__()
         # self.pgsql_conn = psycopg2.connect("dbname=capdb user=capuser password=cap_pass")
-        conf = ConnPgsqlConfig().get_config()
-        self.pgsql_conn = psycopg2.connect(
-            host=conf['url'],
-            port=conf['port'],
-            database=conf['db_name'],
-            user=conf['user'],
-            password=conf['user_pass'])
-        self.cursor = self.pgsql_conn.cursor()
+        try:
+            conf = ConnPgsqlConfig().get_config()
+            self.pgsql_conn = psycopg2.connect(
+                host=conf['url'],
+                port=conf['port'],
+                database=conf['db_name'],
+                user=conf['user'],
+                password=conf['user_pass'])
+            self.cursor = self.pgsql_conn.cursor()
+        except Exception as e:
+            print(f"configuration data not loaded {e}")
 
     def get_pgsql_version(self):
         self.cursor.execute('SELECT version()')
         db_version = self.cursor.fetchone()
         print(db_version)
         self.cursor.close()
+
 
 if __name__ == '__main__':
     connector = ConnPsqlInit()
