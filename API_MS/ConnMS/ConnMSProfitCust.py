38d1afea8c62dff00dcc48f456ca37956282cedb
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-05-15 23:05:28 +0300

    profit by customers init commit

---
+++ API_MS/ConnMS/ConnMSProfitCust.py
@@ -0,0 +1,47 @@
+from API_MS.ConnMS.ConnMSMainClass import ConnMSMainClass
+
+
+
+class ConnMSProfitCust(ConnMSMainClass):
+    """ connector to MoiSklad profit by customers"""
+    request_url = 'url_profit_by_cust_list'
+    """ requested api url"""
+    request_token = 'access_token'
+    """ API token"""
+
+    def __init__(self):
+        super().__init__()
+        self.logger.debug(f"module {__class__.__name__} started")
+        super().set_config(url_conf_key=self.request_url, token_conf_key=self.request_token)
+
+    def get_profit_by_cust(self, from_date=None, to_date=None, to_file=False):
+        """ return dict with profit by customers
+        has 'to_date' parameter
+        date format '2022-12-08' or '2019-07-10 12:00:00'"""
+        param = ""
+        if from_date or to_date:
+            if from_date:
+                param = f"momentFrom={from_date}"
+            if to_date:
+                param += f"&momentTo<={to_date}"
+        else:
+            self.logger.warning(f"{__class__.__name__} request not specified to_date parameter")
+        self.set_api_param_line(param)
+        data_dict = self.get_api_data(to_file=to_file)
+        new_data_dict = dict({"sum": 0.0, "customers": data_dict})
+        for i, prod in enumerate(data_dict["rows"]):
+            # profit - прибыль
+            profit = prod["profit"] / 100
+            # margin -рентабельность
+            margin = prod["margin"]
+            new_data_dict["sum"] = round(new_data_dict.get("sum", 0) + profit, 2)
+        if to_file:
+            from API_MS.ConnMS.ConnMSSaveJson import ConnMSSaveJson
+            ConnMSSaveJson().save_data_json_file(data_dict=new_data_dict, file_name="profit_cust_sum.json")
+        return new_data_dict
+
+
+if __name__ == '__main__':
+    connector = ConnMSProfitCust()
+    data = connector.get_profit_by_cust(from_date="2023-05-01", to_date="2023-05-15", to_file=True)
+    print(data["sum"])
---
+++ API_MS/ContMS/ContMSProfitCust.py
@@ -0,0 +1,17 @@
+from API_MS.ContMS.ContMSMainClass import ContMSMainClass
+from API_MS.ConnMS.ConnMSProfitCust import ConnMSProfitCust
+# import json
+
+
+class ContMSProfitCust(ContMSMainClass, ConnMSProfitCust):
+    """ controller class to get out invoices data"""
+
+    def __init__(self):
+        super().__init__()
+        self.logger.debug(f"module {__class__.__name__} started")
+
+
+if __name__ == '__main__':
+    controller = ContMSProfitCust()
+    data = controller.get_profit_by_cust(from_date="2023-01-01", to_date="2023-02-01", to_file=True)
+    print(data['sum'])
