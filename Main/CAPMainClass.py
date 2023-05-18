083ad10c82ad23efff1a19a147776676b50d5d71
rusttm <rustammazhatov@gmail.com>
2023-05-18 12:17:40 +0300

    local logger starts commit

--- Main/CAPMainClass.py
+++ Main/CAPMainClass.py
@@ -1,23 +1,25 @@
+from Main.CAPLogger import CAPLogger
 import logging
 
 
-class CAPMainClass(object):
+class CAPMainClass(CAPLogger):
     """ main logger file """
-
-    logger = None
-    """ main class logger in CAP project"""
+    # 
+    # logger = None
+    # """ main class logger in CAP project"""
 
     def __init__(self, file_name="cap.log"):
-        self.set_logger("CAPMainClass")
-
-    def set_logger(self, logger_name="main"):
-        self.logger = logging.getLogger(logger_name)
-        self.logger.setLevel(logging.DEBUG)
-        logger_handler = logging.FileHandler(f"{logger_name}.log", mode="w")
-        # logger_handler = logging.FileHandler("controller.log", mode="w")
-        logger_formatter = logging.Formatter("%(asctime)s,%(msecs)d %(name)s %(levelname)s msg: %(message)s")
-        logger_handler.setFormatter(logger_formatter)
-        self.logger.addHandler(logger_handler)
-
-    def get_logger(self):
-        return self.logger
+        super().__init__()
+    #     self.set_logger("CAPMainClass")
+    # 
+    # def set_logger(self, logger_name="main"):
+    #     self.logger = logging.getLogger(logger_name)
+    #     self.logger.setLevel(logging.DEBUG)
+    #     logger_handler = logging.FileHandler(f"{logger_name}.log", mode="w")
+    #     # logger_handler = logging.FileHandler("controller.log", mode="w")
+    #     logger_formatter = logging.Formatter("%(asctime)s,%(msecs)d %(name)s %(levelname)s msg: %(message)s")
+    #     logger_handler.setFormatter(logger_formatter)
+    #     self.logger.addHandler(logger_handler)
+    # 
+    # def get_logger(self):
+    #     return self.logger
