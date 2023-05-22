d670db2aff4c043e73cc153e6cb7ffbef0270e31
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-05-23 01:09:39 +0300

    create empty table works

--- SQLAlchem/ConnAlchem/ConnALMainClass.py
+++ SQLAlchem/ConnAlchem/ConnALMainClass.py
@@ -2,6 +2,8 @@
 
 from SQLAlchem.SQLAlchemMainClass import SQLAlchemMainClass
 import sqlalchemy
+from sqlalchemy.dialects import postgresql
+from sqlalchemy_utils import database_exists, create_database, drop_database
 from sqlalchemy import MetaData, Table, String, Integer, Column, Text, DateTime, Boolean
 from datetime import datetime
 import psycopg2
@@ -13,6 +15,23 @@ class ConnALMainClass(SQLAlchemMainClass):
         super().__init__()
         self.engine = None
 
+    def types_mapper(self, type=None):
+        mapper = dict({
+            "String(255)": postgresql.INET,
+            "Boolean": postgresql.BOOLEAN,
+            "UUID": postgresql.UUID,
+            "Array(Object)": postgresql.ARRAY,
+            "Object": postgresql.JSON,
+            "Meta": postgresql.JSON,
+            "String(4096)": postgresql.TEXT,
+            "Int": postgresql.INTEGER,
+            "MetaArray": postgresql.ARRAY,
+            "String": postgresql.TEXT,
+            "Enum": postgresql.TEXT,
+            "DateTime": postgresql.DATE
+        })
+        return mapper.get(type, postgresql.TEXT)
+
     def create_engine(self):
         from SQLAlchem.ConnAlchem.ConnALConfig import ConnALConfig
         try:
@@ -23,9 +42,11 @@ class ConnALMainClass(SQLAlchemMainClass):
             user = conf.get('user', '')
             password = conf.get('user_pass', '')
             self.logger.debug(f"{__class__.__name__} read data from config")
-            self.engine = sqlalchemy.create_engine(f"postgresql://{user}:{password}@{host},{port}/{database}",
-                                                   echo=True, pool_size=6, max_overflow=10)
-            self.engine.connect()
+            self.url = f"postgresql://{user}:{password}@{host},{port}/{database}"
+            self.url_temp = f"postgresql://{user}:{password}@{host},{port}/"
+            self.engine = sqlalchemy.create_engine(self.url, echo=True, pool_size=6, max_overflow=10)
+            self.conn = self.engine.connect()
+            # self.cur = self.conn.cursor()
             # self.metadata = sqlalchemy.MetaData()
             # print(self.engine)
         except Exception as e:
@@ -53,25 +74,62 @@ class ConnALMainClass(SQLAlchemMainClass):
                 print(f"configuration data not loaded {e}")
                 self.logger.error(f"{__class__.__name__} can't create connector in psycopg2! {e}")
 
+    def delete_database_alchemy(self, db_name=None):
+        if database_exists(self.url_temp+db_name):
+            drop_database(self.url_temp+db_name)
+        else:
+            # Connect the database if exists.
+            self.engine.connect()
+    def create_database_alchemy(self, db_name=None):
+        if not database_exists(self.url_temp+db_name):
+            create_database(self.url_temp+db_name)
+        else:
+            # Connect the database if exists.
+            self.engine.connect()
+
+
     def create_empty_table(self, table_name=None):
         try:
             self.last_metadata = sqlalchemy.MetaData()
             # new_table = sqlalchemy.Table(name=table_name, metadata=self.last_metadata, autoload_replace=True, autoload_with=self.engine)
-            new_table = sqlalchemy.Table(table_name, self.last_metadata)
+            new_table = sqlalchemy.Table(table_name, self.last_metadata,
+                                         autoload_replace=True, autoload_with=self.engine)
             print(repr(self.last_metadata.tables))
             print(repr(self.last_metadata.tables[table_name]))
             print(new_table.columns.keys())
+            self.last_metadata.create_all(self.engine)
+            self.engine.connect()
             return True
         except Exception as e:
             print(e)
             self.logger.warning(f"{__class__.__name__} cant create new empty table error: {e}")
             return False
 
+    def add_column_2table(self, table_name=None, column_dict=None):
+        """  column_dict {"name":"info", "type":"String(255)", "primary_key":False, "default":"None"}"""
+        column_name = column_dict.get("name", "None")
+        column_type = column_dict.get("type", "None")
+        # self.conn.execute(f"ALTER TABLE {table_name} ADD COLUMN {column_name} {column_type};")
+        primary = column_dict.get("primary_key", False)
+        default_value = column_dict.get("default", None)
+        alchemy_type = self.types_mapper(column_type)
+        col = sqlalchemy.Column(column_name, alchemy_type, primary_key=primary, default=default_value)
+        col_name = col.compile(dialect=self.engine.dialect)
+        col_type = col.type.compile(self.engine.dialect)
+
+        self.conn.execute(f"ALTER TABLE {table_name} ADD COLUMN {col_name} {col_type}")
+        print(repr(self.last_metadata.tables))
+        self.engine.connect()
 
 if __name__ == '__main__':
     connector = ConnALMainClass()
     connector.create_engine()
     print(sqlalchemy.__version__)
+    connector.create_database_alchemy(db_name="newdb")
+    connector.delete_database_alchemy(db_name="newdb")
+    table_name = "TestTable"
     # connector.create_database_pgsql(db_name="testdb")
-    connector.create_empty_table(table_name="TestTable")
+    connector.create_empty_table(table_name=table_name)
+    # column_dict = {"name": "info", "type": "String(255)", "primary_key": False, "default": "None"}
+    # connector.add_column_2table(table_name=table_name, column_dict=column_dict)
 
