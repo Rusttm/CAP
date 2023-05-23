8221f01a983b09f76d4f068e3f1d85d947cb7fa4
rusttm <rustammazhatov@gmail.com>
2023-05-23 17:13:51 +0300

    tables with fields information downloaded!

--- Pgsql/ContPgsql/ContPgsqlCreateFieldsTable.py
+++ Pgsql/ContPgsql/ContPgsqlCreateFieldsTable.py
@@ -6,7 +6,13 @@ from Pgsql.ConnPgsql.ConnPgsqlData import ConnPgsqlData
 
 class ContPgsqlCreateFieldsTable(ConnPgsqlTables, ConnPgsqlJson, ContPgsqlMainClass, ConnPgsqlData):
     """ connector for read tables from pgsql database"""
-
+    tables_list = ['product_fields',
+                   'payins_fields', 'payouts_fields',
+                   'packin_fields', 'packout_fields',
+                   'invout_fields', 'invin_fields',
+                   'stockall_fields', 'stockstore_fields',
+                   'customers_bal_fields', 'customers_fields',
+                   'profit_byprod_fields', 'profit_bycust_fields']
     def __init__(self):
         super().__init__()
 
@@ -37,19 +43,22 @@ class ContPgsqlCreateFieldsTable(ConnPgsqlTables, ConnPgsqlJson, ContPgsqlMainCl
             self.put_data_2table(table_name=table_name, col_names_list=col_names_list, col_values_list=col_values_list)
 
     def fill_data_from_json(self):
-        tables_list = ['product_fields',
-                       'payins_fields', 'payouts_fields',
-                       'packin_fields', 'packout_fields',
-                       'invout_fields', 'invin_fields',
-                       'stockall_fields', 'stockstore_fields',
-                       'customers_bal_fields', 'customers_fields',
-                       'profit_byprod_fields', 'profit_bycust_fields']
-        pass
+        for file_name in self.tables_list:
+            self.create_table_from_json_field(file_name=file_name)
+            print(f"created table {file_name}")
+
+    def delete_all_tables(self):
+        for file_name in self.tables_list:
+            self.delete_table(table_name=file_name)
+            print(f"deleted table {file_name}")
+
+
 
 
 
 if __name__ == '__main__':
     connector = ContPgsqlCreateFieldsTable()
     # print(f"tables list {connector.get_tables_list()}")
-    print(f"try to create table from 'product_fields.json' result - {connector.create_table_from_json_field(file_name='product_fields.json')}")
-    print(connector.get_full_data(table_name='product_fields'))
+    # print(f"try to create table from 'product_fields.json' result - {connector.create_table_from_json_field(file_name='product_fields.json')}")
+    # print(connector.get_full_data(table_name='product_fields'))
+    print(connector.fill_data_from_json())
