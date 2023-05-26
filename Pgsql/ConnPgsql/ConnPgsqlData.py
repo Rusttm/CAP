72e57103a873a4a64ddf7983a52b69029f411bb0
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-05-26 10:51:18 +0300

    multiple rows handler added

--- Pgsql/ConnPgsql/ConnPgsqlData.py
+++ Pgsql/ConnPgsql/ConnPgsqlData.py
@@ -71,6 +71,18 @@ class ConnPgsqlData(ConnPgsqlMainClass):
             # print(e)
             self.logger.error(f"{__class__.__name__} error while put data to table {table_name}: {e}")
 
+    def put_multiple_data_2table(self, table_name: str, col_names_list: list, col_values_lists: list):
+        # column_string = ', '.join(col_names_list)
+        col_values_string = self.values_in_request_handler(col_values_lists)
+        col_names_string = self.columns_in_request_handler(col_names_list)
+        req_line = f" INSERT INTO {table_name}  {col_names_string} VALUES {col_values_string}"
+        try:
+            ans = self.send_get_request(req_line=req_line)
+            return ans
+        except Exception as e:
+            # print(e)
+            self.logger.error(f"{__class__.__name__} error while put data to table {table_name}: {e}")
+
     def columns_in_request_handler(self, col_names_list, table_name):
         result_string = '( '
         for i, elem in enumerate(col_names_list):
@@ -83,7 +95,7 @@ class ConnPgsqlData(ConnPgsqlMainClass):
         result_string = result_string.replace("\\", "")
         return result_string
 
-    def values_in_request_handler(self, col_values_list, table_name):
+    def values_in_request_handler(self, col_values_list):
         """ add '{}' for json and
         return corrected list []"""
         temp_array = list()
@@ -95,6 +107,15 @@ class ConnPgsqlData(ConnPgsqlMainClass):
         result_string = result_string.replace("\\'", "'")
         result_string = result_string.replace("json[]'", "json[]")
         result_string = result_string.replace("'array", "array")
+        return result_string
+
+    def values_list_in_request_handler(self, col_values_lists):
+        result_string = '( '
+        for i, values_list in enumerate(col_values_lists):
+            result_string += self.values_in_request_handler(values_list)
+            if i < len(col_values_lists) - 1:
+                result_string += ", "
+        result_string += ' )'
         return result_string
 
 
