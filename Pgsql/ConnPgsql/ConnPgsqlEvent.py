bd5d2f1b028fc55a3d3e037a0f1c4d7c06dfa58a
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-05-28 02:14:47 +0300

    somee corrections

--- Pgsql/ConnPgsql/ConnPgsqlEvent.py
+++ Pgsql/ConnPgsql/ConnPgsqlEvent.py
@@ -9,20 +9,15 @@ class ConnPgsqlEvent(ConnPgsqlMainClass):
     def __init__(self):
         super().__init__()
 
-    def put_event(self, event_dict):
+    def put_event(self, event_dict: dict):
         """ event should be dictionary like {'event_name':'string', 'event_descr':'', 'event_to':''} """
-        if type(event_dict) == dict:
-            from Pgsql.ConnPgsql.ConnPgsqlData import ConnPgsqlData
-            col_names_list = list(event_dict.keys())
-            col_values_list = list(event_dict.values())
-            connector = ConnPgsqlData().put_data_2table(table_name=self.service_table_name,
-                                                        col_names_list=col_names_list,
-                                                        col_values_list=col_values_list)
-
-
-
-
-
+        # if type(event_dict) == dict:
+        from Pgsql.ConnPgsql.ConnPgsqlData import ConnPgsqlData
+        col_names_list = list(event_dict.keys())
+        col_values_list = list(event_dict.values())
+        connector = ConnPgsqlData().put_data_2table(table_name=self.service_table_name,
+                                                    col_names_list=col_names_list,
+                                                    col_values_list=col_values_list)
 
 
 if __name__ == '__main__':
