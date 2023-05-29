15053dc8679b2e018552ef873ebd9740b77f1129
rusttm <rustammazhatov@gmail.com>
2023-05-29 18:27:34 +0300

    pgsql initiator and updater added in CAPmain

--- CAPMain/Start.py
+++ CAPMain/Start.py
@@ -27,21 +27,23 @@ class Main(CAPMainClass):
         self.start_socket_service()
         time.sleep(3)
         self.start_telegrambot_service()
-
+        time.sleep(3)
+        self.start_api_db_updater()
 
 
 if __name__ == '__main__':
     main_class = Main()
     main_class.main()
-    for i in range(100):
+    while True:
         now = time.ctime()
         # memory_system = dict(psutil.virtual_memory()._asdict())
-        memory_msg = f"memory usage: {psutil.virtual_memory().percent}% ({psutil.virtual_memory().used//1073741824})Gb"
+        memory_used = round(psutil.virtual_memory().used/1073741824, 2)
+        memory_msg = f"memory usage: {psutil.virtual_memory().percent}% ({memory_used})Gb"
         cpu_msg = f"cpu usage: {psutil.cpu_percent(interval=None)}%"
-        msg_line = f"Hi telegram {i} msg from admin\n at {now}:\n {memory_msg}\n {cpu_msg}"
+        msg_line = f"CAP msg from admin\n at {now}:\n {memory_msg}\n {cpu_msg}"
         main_class.socket_service.admin_client.send_socket_msg(to_user="telegram",
                                                                msg_text=msg_line)
-        time.sleep(3600)
+        time.sleep(43200)
 
     # msapi1 = ContCAPMS.ContCAPMS()
     # msapi1.get_cont_id()
