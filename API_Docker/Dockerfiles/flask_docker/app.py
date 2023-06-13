d7241d80594291e8b1c2d8b008f4f3e9b098f5b9
rusttm <rustammazhatov@gmail.com>
2023-06-13 17:48:14 +0300

    flask in docker works and gets info from configfile

--- API_Docker/Dockerfiles/flask_docker/app.py
+++ API_Docker/Dockerfiles/flask_docker/app.py
@@ -1,12 +1,30 @@
 from flask import Flask
 import os
-flask_token = os.environ['FLASK_TOKEN']
+import configparser
 
 
+# flask_token = os.environ['FLASK_TOKEN']
+flask_token = 'XXXX'
 app = Flask(__name__)
+
+dir_name = "config"
+file_name = "flask.ini"
+sector = "FLASK"
+var = "FLASK_TOKEN2"
+
+def get_from_ini():
+    try:
+        conf = configparser.ConfigParser()
+        # FILE_DIR = os.path.dirname(os.path.dirname(__file__))
+        FILE_DIR = os.path.dirname(__file__)
+        CONF_FILE_PATH = os.path.join(FILE_DIR, dir_name, file_name)
+        conf.read(CONF_FILE_PATH)
+        return conf[sector][var]
+    except Exception as e:
+        return e
 
 
 
 @app.route('/')
 def hello_world():
-    return f'Hello, Docker! token is: {flask_token}'
+    return f'Hello, Docker! token is: {flask_token} ini file {get_from_ini()}'
