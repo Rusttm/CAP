3805c8de8eae6996c2905fa7b3217b0eccabbe17
rusttm <rustammazhatov@gmail.com>
2023-07-02 20:29:27 +0300

    test runner for airflow updater

--- Airflow/airdocker/dags/db_updater_telegram.py
+++ Airflow/airdocker/dags/db_updater_telegram.py
@@ -21,25 +21,63 @@ Example use of Telegram operator.
 from __future__ import annotations
 
 import os
-
 from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
 from airflow import DAG
 from airflow.providers.telegram.operators.telegram import TelegramOperator
+from airflow.operators.bash_operator import BashOperator
+
+import sys
+sys.path.append('/home/rusttm/PycharmProjects/CAP/Pgsql')
+# from Pgsql.PgsqlUpdaterAir import PgsqlUpdaterAir
+
+def telegram_on_fail(context):
+    alarm_text = f"""\U0001F914: Task Failed.
+             *Task*: {context.get('task_instance').task_id}
+             *Dag*: {context.get('task_instance').dag_id}
+             *Execution Time *: {context.get('execution_date')}
+             *Exception*: {context.get('exception')}
+             *Log Url*: {context.get('task_instance').log_url}
+             """
+
+    failed_alert = TelegramOperator(
+        task_id=f"task_telegram_on_failure{VERSION}",
+        telegram_conn_id="telegram_default",
+        token=get_token()[0],
+        chat_id=get_token()[1],
+        text=alarm_text
+    )
+    return failed_alert.execute(context=context)
 
 
 default_args = {
     'owner': 'rusttm',
     'retry': 5,
     'retry_delay': timedelta(minutes=5),
-    'catchup': False
+    'catchup': False,
+    'on_failure_callback': telegram_on_fail
 }
-
+VERSION = 5
 ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
-DAG_ID = "test_telegram_v6"
+DAG_ID = f"test_updater_v{VERSION}"
 
 def get_text_4tgbot():
-    return f"information from airflow for bot: time.now {datetime.now().strftime('%y:%m:%d %H:%M:%S')}"
+    import platform
+    import glob
+    # from Pgsql.PgsqlUpdaterAir import PgsqlUpdaterAir
+    text_line = ""
+    text_line += "///".join(glob.glob("."))
+    sys_os = platform.platform()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    # os.environ['PATH'] += ':' + dir_path
+    # sys.path.insert(0, dir_path)
+    # sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+    text_line = f"Current project path: {dir_path} added to System: {sys_os}, system paths {sys.path}"
+    text_line += f"information from airflow for bot: time.now {datetime.now().strftime('%y:%m:%d %H:%M:%S')}"
+    # print(os.path.realpath(__file__))
+    text_line += os.path.realpath(__file__)
+    return text_line
+
 
 def get_token() -> tuple:
     import configparser
@@ -59,18 +97,24 @@ def get_token() -> tuple:
 with DAG(default_args=default_args,
          dag_id=DAG_ID,
          tags=["example"],
-         start_date=datetime(2023, 6, 26, 14, 45),
+         start_date=datetime(2023, 7, 2, 14, 50), # only UTC time
          max_active_runs=1,
          concurrency=4,
-         schedule_interval=timedelta(minutes=5),  # or '@hourly'  # or '* */1 * * *' from https://crontab.guru/#0_1_*_*_*
-         dagrun_timeout=timedelta(seconds=5)
+         schedule_interval=timedelta(minutes=5),
+         # or '@hourly'  # or '* */1 * * *' from https://crontab.guru/#0_1_*_*_*
+         dagrun_timeout=timedelta(seconds=60)
          ) as dag:
 
     send_message_telegram_task = TelegramOperator(
-        task_id="task_send_message_telegram_v6",
+        task_id=f"task_update_db_v{VERSION}",
         telegram_conn_id="telegram_default",
         token=get_token()[0],
         chat_id=get_token()[1],
         text=get_text_4tgbot(),
         dag=dag
     )
+
+    # task_updater = BashOperator(
+    #     task_id="bash_task_updater",
+    #     bash_command="ls -ial",
+    #     dag=dag)
