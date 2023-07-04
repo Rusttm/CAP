5095f6fcea3cb43f5d840ac3405a7425c8c1c451
rusttm <rustammazhatov@gmail.com>
2023-07-05 00:10:04 +0300

    django chart test commit

--- Web/CAPDjango/CAPDjango/urls.py
+++ Web/CAPDjango/CAPDjango/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
 
 urlpatterns = [
+    path('', views.chart, name='chart'),
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
 ]
