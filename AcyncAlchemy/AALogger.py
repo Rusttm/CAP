eadb626ddc9f5c58ebb660b6ee7ac665036e8b44
rusttm <rustammazhatov@gmail.com>
2023-07-07 18:18:39 +0300

    clear loggers duplicates

--- AcyncAlchemy/AALogger.py
+++ AcyncAlchemy/AALogger.py
@@ -8,7 +8,8 @@ class AALogger(object):
     def __init__(self):
         self.logger = logging.getLogger(self.logger_name)
         self.logger.setLevel(logging.DEBUG)
-        logger_handler = RotatingFileHandler(f"{self.logger_name}.log", mode="w", maxBytes=5 * 1024 * 1024)
+        if self.logger.hasHandlers(): self.logger.handlers.clear()
+        logger_handler = RotatingFileHandler(f"{self.logger_name}.log", mode="a",  backupCount=0, maxBytes=10*1024*1024, delay=0)
         # logger_handler = logging.FileHandler("controller.log", mode="w")
         logger_formatter = logging.Formatter("%(asctime)s,%(msecs)d %(name)s %(levelname)s msg: %(message)s")
         logger_handler.setFormatter(logger_formatter)
