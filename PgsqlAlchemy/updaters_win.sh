7d74b3b9703df926fb1fbcf262eba46dcc79eee0
Rustam <rustammazhatov@gmail.com>
2023-07-18 13:11:31 +0100

    bash updater for windows

---
+++ PgsqlAlchemy/updaters_win.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "hi everybody"
+
+# set -e
+# alchemy_env/Scripts/activate.ps1
+C:/Users/User/Desktop/CAP/CAP/PgsqlAlchemy/alchemy_env/Scripts/python C:\Users\User\Desktop\CAP\CAP\PgsqlAlchemy\ModALUpdaters\ModALUpdater.py
+
+# python manage.py runserver 127.0.0.1:8083
