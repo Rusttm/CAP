a87f1726bcc6bf128293473d83892fbea7f4b967
rusttm <rustammazhatov@gmail.com>
2023-07-25 01:24:15 +0300

    some corrections in logger properties

--- PgsqlAlchemy/PgsqlAlchemyLogger.py
+++ PgsqlAlchemy/PgsqlAlchemyLogger.py
@@ -10,8 +10,7 @@ class PgsqlAlchemyLogger(object):
         self.logger = logging.getLogger(self.logger_name)
         self.logger.setLevel(logging.DEBUG)
         if self.logger.hasHandlers(): self.logger.handlers.clear()
-        logger_handler = RotatingFileHandler(f"{self.logger_name}.log", mode="a", backupCount=0,
-                                             maxBytes=2 * 1024 * 1024, delay=0)
+        logger_handler = RotatingFileHandler(f"{self.logger_name}.log", mode="a", backupCount=0, maxBytes=2 * 1024 * 1024, delay=False)
         # logger_handler = logging.FileHandler("controller.log", mode="w")
         logger_formatter = logging.Formatter("%(asctime)s,%(msecs)d %(name)s %(levelname)s msg: %(message)s")
         logger_handler.setFormatter(logger_formatter)
