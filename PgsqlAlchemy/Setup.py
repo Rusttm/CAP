cf5f3a010dcdc2a4148b2dfbbf04f3517afc9d29
rusttm <rustammazhatov@gmail.com>
2023-12-27 14:57:32 +0300

    new api requirements

---
+++ PgsqlAlchemy/Setup.py
@@ -0,0 +1,11 @@
+import sys
+import os
+import platform
+
+sys_os = platform.platform()
+dir_path = os.path.dirname(os.path.realpath(__file__))
+# os.environ['PATH'] += ':' + dir_path
+# sys.path.insert(0, dir_path)
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(f"Current project path: {dir_path} added to System: {sys_os}")
+print(os.path.realpath(__file__))
