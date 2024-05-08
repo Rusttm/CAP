b232c8d05e5aaf2b0c76290a5f26c7d773679c26
rusttm <rustammazhatov@gmail.com>
2024-05-08 17:18:27 +0300

    corrections in updater class

--- PgsqlAlchemy/ModALUpdaters/ModALUpdater.py
+++ PgsqlAlchemy/ModALUpdaters/ModALUpdater.py
@@ -14,6 +14,7 @@ class ModALUpdater(ModALUpdaterMainClass):
     models_dir = os.path.join("config", "models")
     gen_module = "PgsqlAlchemy.ModALGen"
     allowed_arguments = ["daily", "hourly", "ondemand", "test"]
+    model_dict = None   # dictionary from model json
     def __init__(self):
         super().__init__()
 
@@ -37,56 +38,67 @@ class ModALUpdater(ModALUpdaterMainClass):
         # from every json config reads type of refreshing table
         for model in all_models:
             # get all data from file
-            model_dict = json_loader.get_data_from_json(file_name=model, dir_name=self.models_dir)
+            self.model_dict = json_loader.get_data_from_json(file_name=model, dir_name=self.models_dir)
             # get update property: daily, hourly, ondemand
-            updated_key = model_dict.get("updated", None)
+            updated_key = self.model_dict.get("updated", None)
             # if it not equal requested - skips
             if updated_key != period:
                 # skip table if period not equal table period
                 continue
-            # other points: "ondemand", "test"
+            # other points: "ondemand", "test" - generates tables, not MS API requested
             if updated_key in ["ondemand", "test"]:
-                # for this updaterd read class + method and run it
-                updater_class_name = model_dict.get("updater_class", None)
-                updater_function_name = model_dict.get("updater_func", None)
-                # make and run updater function for ModALGen
-                module_str = f"{self.gen_module}.{updater_class_name}"
-                module = importlib.import_module(module_str)
-                updater_class = getattr(module, updater_class_name)
-                updater_class_obj = updater_class()
-                updater_conn = getattr(updater_class_obj, updater_function_name)
-                # create dict with parameters
-                request_dict = {
-                    "model_class_table": model_dict.get("table", None),
-                    "updater_class_name": updater_class_name,
-                    "updater_function_name": updater_function_name,
-                    "unique_col": model_dict.get("unique_col", None),
-                    "date_field": model_dict.get("date_field", None),
-                    "config_url": model_dict.get("config_url", None),
-                    "model_tables": model_dict.get("model_tables", None),
-                    "service_url": model_dict.get("service_url", None),
-
-                }
-                ans = updater_conn(**request_dict)
-                ans_list.append(dict({model_dict.get("table", None): ans}))
+                ans = self.generated_tables_updater()
+                ans_list.append(dict({self.model_dict.get("table", None): ans}))
 
             # if daily key -> run updater
             elif updated_key in ["daily", "hourly"]:
+                """ this part for hourly/daily requests in MS 
+                you must set 'url' of MS_API in json """
                 # load from json keys
-                model_class_table = model_dict.get("table", "unknown_table")
-                model_class_name = model_dict.get("model_class", None)
-                model_unique_col = model_dict.get("unique_col", None)
+                # it uses updaters by ModALUpdTable
+                model_class_table = self.model_dict.get("table", "unknown_table")
+                model_class_name = self.model_dict.get("model_class", None)
+                model_unique_col = self.model_dict.get("unique_col", None)
                 ans = updater_module.update_model_table(model_class_name=model_class_name,
                                                         model_unique_col=model_unique_col,
                                                         model_class_table=model_class_table)
-                ans_list.append(dict({model_dict.get("table", "unknown_table"): ans}))
+                ans_list.append(dict({self.model_dict.get("table", "unknown_table"): ans}))
 
         return ans_list
 
+    def generated_tables_updater(self) -> dict:
+        """ this updater reads from json file:
+        class ('updater_class') + method ('updater_func') and runs it """
+        updater_class_name = self.model_dict.get("updater_class", None)
+        updater_function_name = self.model_dict.get("updater_func", None)
+        # makes and run updater function (updater_conn)
+        # with class generator in self.gen_module = "PgsqlAlchemy.ModALGen"
+        module_str = f"{self.gen_module}.{updater_class_name}"
+        module = importlib.import_module(module_str)
+        updater_class = getattr(module, updater_class_name)
+        updater_class_obj = updater_class()
+        updater_conn = getattr(updater_class_obj, updater_function_name)
+        # create dict with parameters
+        request_dict = {
+            "model_class_table": self.model_dict.get("table", None),
+            "updater_class_name": updater_class_name,
+            "updater_function_name": updater_function_name,
+            "unique_col": self.model_dict.get("unique_col", None),
+            "date_field": self.model_dict.get("date_field", None),
+            "config_url": self.model_dict.get("config_url", None),
+            "model_tables": self.model_dict.get("model_tables", None),
+            "service_url": self.model_dict.get("service_url", None),
+
+        }
+        ans = updater_conn(**request_dict)
+        return ans
+
     def daily_updater(self):
+        """ updates only MS API requests"""
         return self.db_updater(period="daily")
 
     def hourly_updater(self):
+        """ updates only MS API requests"""
         return self.db_updater(period="hourly")
 
     def ondemand_updater(self):
@@ -137,7 +149,7 @@ if __name__ == '__main__':
     # print(f"daily updates ({round(time.time() - time1, 2)}sec) result: {res}")
 
     # res = updater.db_updater(period="test")
-    res = updater.db_updater(period="test")
+    res = updater.db_updater(period="ondemand")
     print(f"ondemand updates ({round(time.time() - time1, 2)}sec) result: {res}")
 
     print(f"function time = {round(time.time() - start, 2)}sec")
